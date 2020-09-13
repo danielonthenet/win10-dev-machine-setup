bc734a1675269b9e3326c8096d16ab8b1edfe472
Daniel Moses <danielonthenet@live.com>
2020-09-14 00:16:10 +0800

    Fix choco install errors

--- 01_Base.ps1
+++ 01_Base.ps1
@@ -1,1 +1,8 @@
-choco install choco-packages.config -y
+If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))
+{
+  # Relaunch as an elevated process:
+  Start-Process powershell.exe "-File",('"{0}"' -f $MyInvocation.MyCommand.Path) -Verb RunAs
+  exit
+}
+
+choco install $PSScriptRoot\choco-packages.config -y
